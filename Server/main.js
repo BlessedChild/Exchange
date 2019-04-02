cd7a434a661d4a67b0f4e8fb94a7b0467905478b
Null <null@NulldeiMac.local>
2019-04-02 13:48:32 +0800

    update

--- Server/main.js
+++ Server/main.js
@@ -4,13 +4,23 @@ var app = express()
 // import grobal config parameter
 var config = require('./config.json');
 
+// import render engine "slog"
+var slog = require('slog-engine');
+app.engine('slog', slog()); // define the template engine
+app.set('views', './views') // specify the views directory
+app.set('view engine', 'slog') // register the template engine
+
 // import middleware to log
 var log = require('./middleware/log.js')
 
 app.use(log())
 
-app.get('/', (req, res) => {
-  res.send({"result_status": true, "name": "啊旭"})
+// app.get('/', (req, res) => {
+//   res.send({"result_status": true, "name": "啊旭"})
+// })
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
 app.listen(config.Port)
