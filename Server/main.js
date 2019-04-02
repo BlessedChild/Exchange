9d8ab820ccdee5e608a9b0df4d7662e965f027a1
Null <null@NulldeiMac.local>
2019-04-02 17:00:52 +0800

    update

--- Server/main.js
+++ Server/main.js
@@ -1,5 +1,5 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 // import grobal config parameter
 var config = require('./config.json');
@@ -7,20 +7,20 @@ var config = require('./config.json');
 // import render engine "slog"
 var slog = require('slog-engine');
 app.engine('slog', slog()); // define the template engine
-app.set('views', './views') // specify the views directory
-app.set('view engine', 'slog') // register the template engine
+app.set('views', './views'); // specify the views directory
+app.set('view engine', 'slog'); // register the template engine
 
 // import middleware to log
-var log = require('./middleware/log.js')
+var log = require('arthurslog-log');
 
-app.use(log())
-
-// app.get('/', (req, res) => {
-//   res.send({"result_status": true, "name": "啊旭"})
-// })
+app.use(log());
 
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
+    res.render('index', { title: 'Hey', message: 'Hello there!' });
 })
 
-app.listen(config.Port)
+if(config.Mode == "online"){
+    app.listen(config.Port);
+}else if(config.Mode == "developing"){
+    app.listen(config.DevPort);
+}
